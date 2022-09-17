6cc4865b9ef5f4f57dde066cbbabfaf08778b612
lambohopo <86089421+lambohopo@users.noreply.github.com>
2022-09-17 20:19:45 +0700

    Update promise.js
    Challenge Promise

--- promise.js
+++ promise.js
@@ -1,8 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let dataIXX = await promiseTheaterIXX();
+  let dataVGC = await promiseTheaterVGC();
+  let sumData = dataIXX.concat(dataVGC);
+
+  let countEmosi = 0;
+  sumData.map((data) => data.hasil === emosi && (countEmosi += 1));
+  return countEmosi;
+};
 
 module.exports = {
   promiseOutput,
 };
+
